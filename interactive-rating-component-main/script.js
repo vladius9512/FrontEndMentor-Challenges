0072d87216aeb1a53432b2f62357720e56ecd2b2
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2023-05-07 20:03:26 +0300

    Made the js functions to get the starRating and show it in the end screen

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,9 +1,15 @@
 const mainElem = document.getElementById("main");
+let chosenRating = null;
 
 function createElem(elemType, className, src) {
     const elem = document.createElement(elemType);
-    elem.classList.add = className;
-    elem.src = src;
+    if (className) {
+        elem.classList.add(className);
+    }
+
+    if (src) {
+        elem.src = src;
+    }
     return elem;
 }
 
@@ -15,35 +21,46 @@ function initializeSite() {
     const pElem = createElem("p");
     pElem.innerText =
         "Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!";
-    const fbScore1 = createElem("div", "feedback-score 1");
-    fbScore1.innerText = "1";
-    const fbScore2 = createElem("div", "feedback-score 2");
-    fbScore2.innerText = "2";
-    const fbScore3 = createElem("div", "feedback-score 3");
-    fbScore3.innerText = "3";
-    const fbScore4 = createElem("div", "feedback-score 4");
-    fbScore4.innerText = "4";
-    const fbScore5 = createElem("div", "feedback-score 5");
-    fbScore5.innerText = "5";
+    const fbScoresContainerDiv = createElem("div", "feedback-container");
+    const feedbackElements = [
+        { value: 1 },
+        { value: 2 },
+        { value: 3 },
+        { value: 4 },
+        { value: 5 },
+    ];
+    feedbackElements.forEach((elem) => {
+        const feedbackDOMElement = createElem("div", `feedback-score`);
+        feedbackDOMElement.innerText = elem.value;
+        feedbackDOMElement.addEventListener("click", () => {
+            const activeRating = document.querySelector(
+                ".feedback-score.active"
+            );
+            if (activeRating) {
+                activeRating.classList.remove("active");
+            }
+            feedbackDOMElement.classList.add("active");
+            chosenRating = elem.value;
+        });
+        fbScoresContainerDiv.appendChild(feedbackDOMElement);
+    });
     const btnDiv = createElem("div", "button-container");
     const btnSubmit = createElem("button", "submit");
+
     btnSubmit.innerText = "Submit";
+    btnSubmit.addEventListener("click", () => {
+        if (!chosenRating) {
+            return;
+        }
+        mainElem.innerHTML = "";
+        submitedView(chosenRating);
+    });
     starDiv.appendChild(starImg);
     btnDiv.appendChild(btnSubmit);
-    mainElem.append(
-        starDiv,
-        h1,
-        pElem,
-        fbScore1,
-        fbScore2,
-        fbScore3,
-        fbScore4,
-        fbScore5,
-        btnDiv
-    );
+    mainElem.append(starDiv, h1, pElem, fbScoresContainerDiv, btnDiv);
 }
 
-function submitedView() {
+function submitedView(starRating) {
     const thanksDiv = createElem("div", "thank-you");
     const thanksImg = createElem(
         "img",
@@ -51,6 +68,7 @@ function submitedView() {
         "./images/illustration-thank-you.svg"
     );
     const customerRatingDiv = createElem("div", "customer-rating");
+    customerRatingDiv.innerText = `You selected ${starRating} out of 5`;
     const h2 = createElem("h2");
     h2.innerText = "Thank you!";
     const callDiv = createElem("div", "call-to-action");
@@ -63,4 +81,3 @@ function submitedView() {
 }
 
 initializeSite();
-submitedView();
