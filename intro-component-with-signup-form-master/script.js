9682d5b8557b8102305b07e48fafc7a97611862f
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2023-05-14 10:09:11 +0300

    Added a span for the error field in case the input is empty or wrong. Created the js functions to display the data, accept it or test it for validity and otherwise show an error message

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -3,23 +3,56 @@ const lastName = document.getElementById("lastName");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const submit = document.getElementById("submit");
-
+const inputs = document.querySelectorAll("input");
 submit.addEventListener("click", (e) => {
     e.preventDefault();
-    const firstNameVal = firstName.value;
-    const lastNameVal = lastName.value;
-    const emailVal = email.value;
-    const passwordVal = password.value;
-    if (firstNameVal === "") {
-        alert("Enter a first name");
+    const firstNameValidity = firstName.validity.valid;
+    const lastNameValidity = lastName.validity.valid;
+    const emailValidity = email.validity.valid;
+    const passwordValidity = password.validity.valid;
+    if (
+        firstNameValidity &&
+        lastNameValidity &&
+        emailValidity &&
+        passwordValidity
+    ) {
+        alert("succes");
     }
-    if (lastNameVal === "") {
-        alert("Enter a last name");
-    }
-    if (emailVal === "") {
-        alert("Enter a valid email");
-    }
-    if (passwordVal === "") {
-        alert("Enter a valid password");
-    }
+});
+
+inputs.forEach((input) => {
+    input.addEventListener("keydown", () => {
+        let span = input.previousElementSibling;
+        setTimeout(() => {
+            if (input.value !== "") {
+                span.hidden = true;
+                input.style.borderColor = "";
+            } else {
+                span.hidden = false;
+                input.style.borderColor = "red";
+            }
+        }, 10);
+    });
+});
+
+inputs.forEach((input) => {
+    input.addEventListener(
+        "blur",
+        (e) => {
+            const isValid = e.target.validity.valid;
+            const message = e.target.validationMessage;
+            const connectedValidationId =
+                e.target.getAttribute("aria-describedby");
+            const connectedValidation = connectedValidationId
+                ? document.getElementById(connectedValidationId)
+                : false;
+
+            if (connectedValidation && message && !isValid) {
+                connectedValidation.style.display = "block";
+            } else {
+                connectedValidation.style.display = "none";
+            }
+        },
+        true
+    );
 });
