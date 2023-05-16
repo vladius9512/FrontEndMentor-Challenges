5912a443d49522f67311c53ebb7f45941b719f8f
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2023-05-16 10:36:26 +0300

    Added the link to the final solution challenge inside the index. After testing the js, I updated it so the active states are exactly as in the design. Styled the page for mobile view and desktop view

--- ping-coming-soon-page-master/script.js
+++ ping-coming-soon-page-master/script.js
@@ -8,7 +8,15 @@ buttonNotify.addEventListener("click", () => {
         /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputEmail.value)
     ) {
         connectedValidation.style.display = "none";
-    } else {
+        inputEmail.style.borderColor = "var(--paleblue)";
+    } else if (inputEmail.value === "") {
+        inputEmail.style.borderColor = "var(--lightRed)";
         connectedValidation.style.display = "block";
+        connectedValidation.innerText =
+            "Whoops! It looks like you forgot to add your email";
+    } else {
+        inputEmail.style.borderColor = "var(--lightRed)";
+        connectedValidation.style.display = "block";
+        connectedValidation.innerText = "You must enter a valid email address";
     }
 });
