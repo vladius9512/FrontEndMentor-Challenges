5824f8822ffdfc99b4cefb447416708b8db7470f
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2023-05-22 09:56:48 +0300

    Added the javascript functions that open and closes the menu

--- news-homepage-main/script.js
+++ news-homepage-main/script.js
@@ -1,1 +1,30 @@
-//position fixed for body so it doesnt scroll when menu is up
+const body = document.getElementsByTagName("body")[0];
+const mobileMenuBtn = document.getElementsByClassName(
+    "mobile-menu-button-container"
+)[0];
+const buttonsContainer =
+    document.getElementsByClassName("buttons-container")[0];
+const overlay = document.getElementsByClassName("overlay")[0];
+const closeBtn = document.getElementsByClassName("close-button-container")[0];
+
+mobileMenuBtn.addEventListener("click", () => {
+    openMenu();
+});
+
+closeBtn.addEventListener("click", () => {
+    closeMenu();
+});
+
+function openMenu() {
+    buttonsContainer.classList.remove("closed");
+    overlay.classList.add("active");
+    body.style.position = "fixed";
+    closeBtn.classList.remove("closed");
+}
+
+function closeMenu() {
+    buttonsContainer.classList.add("closed");
+    overlay.classList.remove("active");
+    body.style.position = "unset";
+    closeBtn.classList.add("closed");
+}
