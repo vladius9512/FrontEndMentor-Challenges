f6af1a932007e512c11643b3fbdd7a2520338bad
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2023-06-29 17:09:21 +0300

    Worked on the checkbox and range inputs. They display the correct values. When you click the checkbox, the values swap to yearly/monthly ones. Worked on the range so if you let the slider inbetween two values, it goes to the closer one

--- interactive-pricing-component-main/script.js
+++ interactive-pricing-component-main/script.js
@@ -1,24 +1,79 @@
-let steps = [];
-document.querySelectorAll("#values option").forEach((option) => {
-    steps.push(option.value);
-});
+let steps = ["8.00", "12.00", "16.00", "24.00", "36.00"];
 
+const yearlySteps = ["72.00", "108.00", "144.00", "216.00", "324.00"];
+const pageviews = {
+    8: "10k",
+    12: "50k",
+    16: "100k",
+    24: "500k",
+    36: "1M",
+    72: "10k",
+    108: "50k",
+    144: "100k",
+    216: "500k",
+    324: "1M",
+};
+
+const yearly = document.getElementById("yearly");
+const displayPrice = document.getElementsByClassName("price")[0];
+const displayPageviews = document.getElementsByClassName("pageviews")[0];
 const slider = document.querySelector("#slider");
+yearly.addEventListener("change", (e) => {
+    if (yearly.checked) {
+        slider.min = 72;
+        slider.max = 324;
+        setPrice(72);
+        setPageviews(pageviews[72]);
+    } else {
+        slider.min = 8;
+        slider.max = 36;
+        setPrice(36);
+        setPageviews(pageviews[36]);
+    }
+});
 
 slider.addEventListener("change", (e) => {
-    steps.forEach((v, i) => {
-        if (
-            e.target.value <= parseFloat(v) &&
-            !steps.includes(e.target.value)
-        ) {
-            e.target.value =
-                i > 0
-                    ? parseFloat(v) - e.target.value <
-                      (parseFloat(v) - parseFloat(steps[i - 1])) / 2
-                        ? v
-                        : steps[i - 1]
-                    : v;
-        }
-        console.log("a");
-    });
+    if (yearly.checked) {
+        yearlySteps.forEach((v, i) => {
+            if (
+                e.target.value <= parseFloat(v) &&
+                !yearlySteps.includes(e.target.value)
+            ) {
+                e.target.value =
+                    i > 0
+                        ? parseFloat(v) - e.target.value <
+                          (parseFloat(v) - parseFloat(yearlySteps[i - 1])) / 2
+                            ? v
+                            : yearlySteps[i - 1]
+                        : v;
+            }
+            setPrice(e.target.value);
+            setPageviews(pageviews[parseFloat(e.target.value)]);
+        });
+    } else {
+        steps.forEach((v, i) => {
+            if (
+                e.target.value <= parseFloat(v) &&
+                !steps.includes(e.target.value)
+            ) {
+                e.target.value =
+                    i > 0
+                        ? parseFloat(v) - e.target.value <
+                          (parseFloat(v) - parseFloat(steps[i - 1])) / 2
+                            ? v
+                            : steps[i - 1]
+                        : v;
+            }
+            setPrice(e.target.value);
+            setPageviews(pageviews[parseFloat(e.target.value)]);
+        });
+    }
 });
+
+const setPrice = (price) => {
+    displayPrice.innerText = `$${price}`;
+};
+
+const setPageviews = (pageviews) => {
+    displayPageviews.innerText = `${pageviews}`;
+};
