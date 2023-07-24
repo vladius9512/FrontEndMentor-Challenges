2a6370de13e5a37920e6c218e007a5c0494c6b91
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2023-07-24 18:41:20 +0300

    Added a small onmouseover over the links, to randomize the letters and display the correct name after a small amount of time

---
+++ index.js
@@ -0,0 +1,32 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+const links = document.querySelectorAll("a");
+console.log(links);
+
+links.forEach((link) => {
+    link.onmouseover = (event) => {
+        let iteration = 0;
+
+        clearInterval(interval);
+
+        interval = setInterval(() => {
+            event.target.innerText = event.target.innerText
+                .split("")
+                .map((letter, index) => {
+                    if (index < iteration) {
+                        return event.target.dataset.value[index];
+                    }
+
+                    return letters[Math.floor(Math.random() * 26)];
+                })
+                .join("");
+
+            if (iteration >= event.target.dataset.value.length) {
+                clearInterval(interval);
+            }
+            iteration += 1 / 3;
+        }, 1);
+    };
+});
